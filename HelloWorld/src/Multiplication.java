49d2ad7db34b9bd3748d24815588ed322d350756
harsha <harsha@DESKTOP-K52DO4F>
2022-03-15 12:17:29 +0530

    tables Code

---
+++ HelloWorld/src/Multiplication.java
@@ -0,0 +1,18 @@
+
+public class Multiplication {
+
+public static void main(String[] args) 
+	{
+		int n =5;
+		int m;
+		for(int i=1;i<=n;i++)
+			{
+			for(int j=1;j<=10;j++){
+			m=i*j;
+			System.out.println(i+"*" +j+"=" +m);
+			}
+			System.out.println();
+		}
+		
+	}
+}
