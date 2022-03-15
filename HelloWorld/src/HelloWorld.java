ddf38cc0e3cea05914db5f7b30edc6279b70469b
harsha <harsha@DESKTOP-K52DO4F>
2022-03-15 12:09:39 +0530

    Code Updated

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,16 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("----------Hello Harshith Welcome-----------");
+		System.out.println("----------Hello  Welcome-----------");
+		int a,b,c;
+		a=15;
+		b=45;
+		c=a*b;
+		System.out.println("Answer is :"+" "+c);
+
+		
+		
+		
 
 	}
 
